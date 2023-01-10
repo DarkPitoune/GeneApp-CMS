73fa22b25ed6e4def81deb28c261f3d110de5af3
Pitoune <pierredheb@gmail.com>
2023-01-10 04:21:38 +0100

    try and solve CORS

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://api.genealogie.dhebrail.fr/",
   app: {
     keys: env.array('APP_KEYS'),
   },
