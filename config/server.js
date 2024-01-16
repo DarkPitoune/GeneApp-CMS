50538371f32ff52b0d47f04158bec08770654ccf
DarkPitoune <pierredheb@gmail.com>
2024-01-16 17:54:32 +0100

    make prod work

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: process.env.NODE_ENV === "development" ? "http://localhost:1337/":"https://api.genealogie.dhebrail.fr/",
+  url: "https://api.genealogie.dhebrail.fr/",
   app: {
     keys: env.array('APP_KEYS'),
   },
