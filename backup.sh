6e6bdfbe97dd22fe87db71a12e129edcbd1609b2
Pitoune <pierredheb@gmail.com>
2024-01-17 22:43:21 +0100

    backup final version

--- backup.sh
+++ backup.sh
@@ -17,21 +17,32 @@ mkdir -p "${backup_dir}"
 export_output=$(yarn strapi export --file "${backup_file}" --no-encrypt)
 backup_exit_status=$?
 
-# Function to send email notification
+# Function to send a message to Telegram
+send_message() {
+  curl -s -X POST https://api.telegram.org/bot$TG_API_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID --data-urlencode text="$1"
+}
+
+# Function to send a file to Telegram
+send_file() {
+  echo "Sending file $1"
+  curl -X POST https://api.telegram.org/bot$TG_API_TOKEN/sendDocument -F chat_id=$TG_CHAT_ID -F document=@"$1"
+}
+
+# Function to send notification
 send_notification() {
   if [ $backup_exit_status -eq 0 ]; then
     TOTAL_SIZE=$(echo "$export_output" | grep 'Total' | sed -n 's/.*[[:space:]]\([0-9.]*[[:space:]]MB\).*/\1/p')
 
     # Using a heredoc for the message
-    cat <<EOF
+    send_message "$(cat <<EOF
 ✅ Backup réussie pour l'app de généalogie d'Hébrail.
 🗂️ Taille totale: $TOTAL_SIZE
 EOF
+    )"
+    send_file "${backup_file}.tar.gz"
   else
-    cat <<EOF
-❌ Backup foirée sur l'app de généalogie d'Hébrail, allez voir les logs cron.
-EOF
-  fi | curl -s -X POST https://api.telegram.org/bot$TG_API_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID --data-urlencode text@-
+    send_message "❌ Backup foirée sur l'app de généalogie d'Hébrail, allez voir les logs cron."
+  fi
 }
 
 
